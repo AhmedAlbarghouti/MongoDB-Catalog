2043033cce593434768f6194a289422447cb3206
B-Crolly <35710557+B-Crolly@users.noreply.github.com>
2021-04-04 18:41:19 -0400

    Update forms.py
    removed deprecated fields

--- forms.py
+++ forms.py
@@ -6,25 +6,6 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 categories = []
 
 
-class RegistrationForm(FlaskForm):
-    username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign up')
-
-
-class LoginForm(FlaskForm):
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
-    submit = SubmitField('Login')
-
-
 class AddForm(FlaskForm):
     name = StringField('Product Name',
                        validators=[DataRequired(), Length(min=2, max=20)])
